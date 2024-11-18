8f9fc74ead8655ed639a3b6ae98f2a079af601ca
1234 <a1234@MacBook-Pro.local>
2024-11-18 12:20:37 +0200

    Created project,removed storyboard,removed properties

---
+++ FoodDeliveryAppPro/FoodDeliveryAppPro/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodDeliveryAppPro
+//
+//  Created by 1234 on 18.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
